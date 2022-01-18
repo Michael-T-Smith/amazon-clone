fc5d32eea1b0daee514b7f4bfc86b8e14226e808
Michael Smith <michaels@fireflyint.com>
2022-01-18 15:16:38 -0500

    Finished "add to basket" function

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,7 +1,22 @@
 import React from 'react'
 import "../css/product.css"
+import { useStateValue } from '../StateProvider';
 
 function Product({id, title, image, price, rating}) {
+  const [{basket}, dispatch] = useStateValue();
+  const addToBasket = () => {
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        title:title,
+        image: image,
+        price: price, 
+        rating: rating
+      }
+    })
+  };
+ 
   return (
     <div className="product">
       <div className="product__info">
@@ -18,7 +33,7 @@ function Product({id, title, image, price, rating}) {
       </div>
 
       <img src={image} alt="Product" />
-      <button>Add to basket</button>
+      <button onClick={addToBasket}>Add to basket</button>
     </div>
   )
 }
